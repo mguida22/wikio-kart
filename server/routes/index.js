fe2254c5413923755f167dfa34c427d38aba7a02
mguida22 <michael.guida@colorado.edu>
2016-02-20 23:43:34 -0700

    express error

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,7 @@ const router = express.Router();
 /* GET home page. */
 
 router.get('/', (req, res) => {
-  res.sendfile(path.join(__dirname, '../public/views/wikirace.html'));
+  res.sendFile(path.join(__dirname, '../public/views/wikirace.html'));
 });
 
 module.exports = router;
